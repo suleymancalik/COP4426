c0d9cc477dba9e88cbe3a9ca941fb272febb94a1
Suleyman Calik <suleymancalik@gmail.com>
2015-02-26 11:31:55 +0200

    Playgrounds

---
+++ Playgrounds/1-Variables.playground/section-1.swift
@@ -0,0 +1,81 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var str2:String = "Merhaba"
+
+
+var str3:String
+
+
+var str4:String?
+
+
+
+str
+str2
+//str3
+str4
+
+
+
+
+
+let possibleNumber = "123"
+
+var convertedNum = possibleNumber.toInt()
+
+
+if let realNumber:Int = convertedNum {
+    realNumber
+}
+else {
+    "sayıya çevrilemedi!"
+}
+
+
+
+if convertedNum == nil {
+    "sayıya çevrilemedi!"
+}
+else {
+    let realNumber:Int = convertedNum!
+    realNumber
+}
+
+
+//var realNumber:Int = convertedNum!
+//
+//realNumber
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Playgrounds/2-Functions.playground/section-1.swift
@@ -0,0 +1,57 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+
+////////////////////////////////////
+func merhabaDe() {
+    "Merhaba"
+}
+
+merhabaDe()
+
+
+
+////////////////////////////////////
+func merhabaDe(isim:String) {
+    "merhaba " + isim
+}
+
+
+merhabaDe("suleyman")
+
+
+
+////////////////
+//// Birine birşey söylemeye yarar
+func birseySoyle(cümle:String , isim:String) {
+    cümle + " " + isim
+}
+
+
+birseySoyle("selam!", "süleyman")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
