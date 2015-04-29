80879ff7f7cefcfa035e7866192791ee428ad3d5
Suleyman Calik <suleymancalik@gmail.com>
2015-04-29 16:59:18 +0300

    CollectionView class

---
+++ Projects/7-CollectionView/CollectionView/PhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.swift
+//  CollectionView
+//
+//  Created by Suleyman Calik on 29/04/15.
+//  Copyright (c) 2015 BAU. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photo:UIImageView!
+
+}
---
+++ Projects/7-CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Suleyman Calik on 29/04/15.
+//  Copyright (c) 2015 BAU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+}
+
